a19c696dab1015c5f91684e334219e204d52a096
Shafwan Eksa Jayadi <shafwaneksajayadi@student.uns.ac.id>
2022-09-22 19:57:37 +0700

    commit

--- CRUD Kependudukan/index.php
+++ CRUD Kependudukan/index.php
@@ -352,7 +352,7 @@ if (isset($_POST['Hapus'])){
     <!-- /.content-wrapper -->
     <footer class="main-footer">
       <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
+        <b>Version</b> 3.2.0.
       </div>
       <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
     </footer>
